b4f0486d4876ab66fb0bb6b122243bac3ffc68b8
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-02-22 14:57:31 +0300

    This commit will add a js function that grades the performance of the user on the test

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,29 @@
 //USER INTERFACE LOGIC
 $(document).ready (function(){
     $("form#quiz").submit(function(){
+        event.preventDefault();
         var name = $("input#username").val();
         var q1 = parseInt($("input:radio[name=A1]:checked").val());
         var q2 = parseInt($("input:radio[name=A2]:checked").val());
         var q3 = parseInt($("input:radio[name=A3]:checked").val());
         var q4 = parseInt($("input:radio[name=A4]:checked").val());
-    });
+        var marks = grade(q1,q2,q3,q4);
+        grading(marks);
+        
+});
 });
 
+//BUSINESS LOGIC
+var grade = function (num1,num2,num3,num4) {
+    return (num1+num2+num3+num4)/20*100;
+};
+
+function grading (total){
+if (total>80) {
+    $("#output").text("You have passed");
+} else if (total>50 && total<80) {
+    $("#output").text("You have fairly passed");
+} else if (total<80) {
+    $("#output").text("You have failed. Retake");
+}
+};
