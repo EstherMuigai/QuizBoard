1d0d7858ed9189aa63efaab638aa859253202d64
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-02-22 16:23:45 +0300

    This commit will add hide and show methods in the UI logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,24 @@
 //USER INTERFACE LOGIC
 $(document).ready (function(){
-    $("form#quiz").submit(function(){
+    $("div#results").hide();
+    $("form#quiz").hide();
+    $("form#details").submit(function(){
         event.preventDefault();
-        var name = $("input#username").val();
-        var q1 = parseInt($("input:radio[name=A1]:checked").val());
-        var q2 = parseInt($("input:radio[name=A2]:checked").val());
-        var q3 = parseInt($("input:radio[name=A3]:checked").val());
-        var q4 = parseInt($("input:radio[name=A4]:checked").val());
-        var marks = grade(q1,q2,q3,q4);
-        grading(marks);
-        
-});
+        $("form#details").hide();
+        $("form#quiz").show();
+        $("form#quiz").submit(function(){
+            event.preventDefault();
+            var name = $("input#username").val();
+            var q1 = parseInt($("input:radio[name=A1]:checked").val());
+            var q2 = parseInt($("input:radio[name=A2]:checked").val());
+            var q3 = parseInt($("input:radio[name=A3]:checked").val());
+            var q4 = parseInt($("input:radio[name=A4]:checked").val());
+            var marks = grade(q1,q2,q3,q4);
+            $("form#quiz").hide();
+            $("div#results").show();
+            grading(marks);
+            });
+        });
 });
 
 //BUSINESS LOGIC
